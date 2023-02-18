76c23cdb8797e7a65a70ec247f65a869a4b2ef01
jfi22496 <conor.wild@sky.com>
2023-02-18 19:53:49 +0000

    First commit

--- constants.py
+++ constants.py
@@ -3,12 +3,12 @@ OPTIONS_FILE = "options.json"
 DATASET_FILE = "dataset.json"
 RSYNC_COMMAND = "rsync -rlpt -v -z --delete rsync.ebi.ac.uk::pub/databases/pdb/data/{datatype_dir}/ \"{data_dir}/{datatype_dir}/\""
 STRUCTURE_FACTORS = (
+    ('pdbx_FWT', 'pdbx_PHWT'),
     ("FWT", "PHWT"),
     ("2FOFCWT", "PH2FOFCWT"),
     ("2FOFCWT_iso-fill", "PH2FOFCWT_iso-fill"),
     ("2FOFCWT_fill", "PH2FOFCWT_fill",),
     ("2FOFCWT", "PHI2FOFCWT"),
-    ('pdbx_FWT', 'pdbx_PHWT')
 )
 PDB_REGEX = "pdb([a-zA-Z0-9]+)\.ent\.gz"
 MTZ_REGEX = "r([a-zA-Z0-9]+)sf\.ent\.gz"
