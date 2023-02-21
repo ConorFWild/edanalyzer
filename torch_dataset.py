b3c1544879804a2246aa97ff6b031a98a3af1c68
jfi22496 <conor.wild@sky.com>
2023-02-21 14:22:30 +0000

    First commit

--- torch_dataset.py
+++ torch_dataset.py
@@ -133,7 +133,8 @@ def get_sample_transform_from_event(event: PanDDAEvent,
 
     # Generate random translation transform
     rng = default_rng()
-    random_translation = (rng.random(3) - 0.5) * 2 * translation
+    # random_translation = (rng.random(3) - 0.5) * 2 * translation
+    random_translation = np.array([0.0,0.0,0.0])
     logger.debug(f"Random translation: {random_translation}")
     random_translation_transform = gemmi.Transform()
     random_translation_transform.vec.fromlist(random_translation.tolist())
