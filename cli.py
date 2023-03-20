bf3bd4b36d51e27ad73f8ad864ff6daa4a2724a1
jfi22496 <conor.wild@sky.com>
2023-03-20 15:07:37 +0000

    Database

--- cli.py
+++ cli.py
@@ -1780,7 +1780,7 @@ def update_from_annotations_v2_get_annotations(
             continue
 
         # Check event map path matches
-        observed_event_map_path = pandda_path / constants.PANDDA_PROCESSED_DATASETS_DIR / constants.PANDDA_EVENT_MAP_TEMPLATE.format(
+        observed_event_map_path = pandda_path / constants.PANDDA_PROCESSED_DATASETS_DIR / str(dtag) / constants.PANDDA_EVENT_MAP_TEMPLATE.format(
             dtag=str(dtag),
             event_idx=str(1),
             bdc=str(1-round(bdc, 2)),
