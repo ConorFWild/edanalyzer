9288ec62179affda2caef92279c700afcea4ecdc
jfi22496 <conor.wild@sky.com>
2023-05-17 19:04:08 +0100

    get_model_map now masks symmetry

--- src/edanalyzer/torch_dataset.py
+++ src/edanalyzer/torch_dataset.py
@@ -427,6 +427,7 @@ def get_model_map(event: PanDDAEvent, xmap_event):
                                 )
                                 num_sym += 1
 
+
     time_finish_ns = time.time()
     print(
         f"Num: {num} : num sym: {num_sym} in {round(time_finish_ns-time_begin_ns, 2)} : {round(event.x, 2)} : {round(event.y, 2)} : {round(event.z, 2)} : {pandda_input_pdb}"
