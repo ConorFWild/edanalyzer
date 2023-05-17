35e5c63a9fb2f91307cad0105a11d2dfe7d4ba9f
jfi22496 <conor.wild@sky.com>
2023-05-17 18:35:55 +0100

    get_model_map now masks symmetry

--- src/edanalyzer/torch_dataset.py
+++ src/edanalyzer/torch_dataset.py
@@ -341,14 +341,19 @@ def get_model_map(event: PanDDAEvent, xmap_event):
 
     num_sym = 0
     num = 0
-    marks = ns.find_atoms(gemmi.Position(event.x, event.y, event.z), '\0', radius=17)
+    event_pos = gemmi.Position(event.x, event.y, event.z)
+    marks = ns.find_atoms(event_pos, '\0', radius=17)
     # print(marks)
     # print(len(marks))
+    mark_dists = []
     for _mark in marks:
         # print(_mark)
         # print(dir(_mark))
         _cra = _mark.to_cra(structure[0])
         mark_pos = gemmi.Position(_mark.x, _mark.y, _mark.z)
+
+        mark_dists.append(event_pos.dist(mark_pos))
+
         if mark_pos.dist(_cra.atom.pos) < 0.1:
             new_xmap.set_points_around(
                                 mark_pos,
@@ -368,6 +373,7 @@ def get_model_map(event: PanDDAEvent, xmap_event):
     print(
         f"Num: {num} : num sym: {num_sym} in {round(time_finish_ns-time_begin_ns, 2)} : {round(event.x, 2)} : {round(event.y, 2)} : {round(event.z, 2)} : {pandda_input_pdb}"
     )
+    print(mark_dists[:10])
 
 
     return new_xmap
