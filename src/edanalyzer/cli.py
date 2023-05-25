a56ee34c9805632ae65617197c9c6c29cd563e6e
jfi22496 <conor.wild@sky.com>
2023-05-25 21:58:47 +0100

    Added squeezenet training option

--- src/edanalyzer/cli.py
+++ src/edanalyzer/cli.py
@@ -873,6 +873,7 @@ def train_pandda_from_dataset_ligand(
             # forward + backward + optimize
             begin_annotate = time.time()
             model_annotation = model(image_c)
+            print(model_annotation)
             finish_annotate = time.time()
             # logger.debug(f"Annotated 12 datasets in {finish_annotate - begin_annotate}")
             # print(outputs.to("cpu").detach().numpy())
