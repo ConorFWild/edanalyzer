db8e021614147687affe230804feb3cdfaa68d50
jfi22496 <conor.wild@sky.com>
2023-05-25 22:43:30 +0100

    Added squeezenet training option

--- src/edanalyzer/torch_network_mobilenet.py
+++ src/edanalyzer/torch_network_mobilenet.py
@@ -4,12 +4,12 @@ from typing import Any, Callable, List, Optional, Sequence
 import torch
 from torch import nn, Tensor
 
-from ..ops.misc import Conv3dNormActivation, SqueezeExcitation as SElayer
-from ..transforms._presets import ImageClassification
-from ..utils import _log_api_usage_once
-from ._api import register_model, Weights, WeightsEnum
-from ._meta import _IMAGENET_CATEGORIES
-from ._utils import _make_divisible, _ovewrite_named_param, handle_legacy_interface
+from torchvision.ops.misc import Conv3dNormActivation, SqueezeExcitation as SElayer
+from torchvision.transforms._presets import ImageClassification
+from torchvision.utils import _log_api_usage_once
+from torchvision.models._api import register_model, Weights, WeightsEnum
+from torchvision.models._meta import _IMAGENET_CATEGORIES
+from torchvision.models._utils import _make_divisible, _ovewrite_named_param, handle_legacy_interface
 
 
 __all__ = [
