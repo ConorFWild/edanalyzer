5df3cf629aeb5d40392051861589ace1aab0bbe2
jfi22496 <conor.wild@sky.com>
2023-07-18 10:37:57 +0100

    pony version

--- src/edanalyzer/cli.py
+++ src/edanalyzer/cli.py
@@ -4190,6 +4190,8 @@ def _get_model_scores(_model, _matched_events):
         events_pyd.append(event_pyd)
     dataset = PanDDAEventDataset(pandda_events=events_pyd)
     records = get_annotations_from_dataset_ligand(dataset, _model)
+    for record in records:
+        print(record)
 
     return records
 
