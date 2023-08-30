8a7e1f0581f1a3ae1c78a5ae1fd84dd12ecaed29
jfi22496 <conor.wild@sky.com>
2023-08-30 14:02:33 +0100

    Cleanerr cli

--- scripts/run.py
+++ scripts/run.py
@@ -12,29 +12,42 @@ from edanalyzer.database_pony import db, EventORM  # import *
 
 @dataclasses.dataclass
 class ConfigTrain:
-    def __init__(self, dic):
-        self.max_epochs = dic['max_epochs']
+    max_epochs: int
+    # def __init__(self, dic):
+    #     self.max_epochs = dic['max_epochs']
 
 
 @dataclasses.dataclass
 class ConfigTest:
-    def __init__(self, dic):
-        self.test_interval = dic['test_interval']
-        self.test_convergence_interval = dic['test_convergence_interval']
+    test_interval: int
+    test_convergence_interval: int
+    # def __init__(self, dic):
+    #     self.test_interval = dic['test_interval']
+    #     self.test_convergence_interval = dic['test_convergence_interval']
 
 
 @dataclasses.dataclass
 class Config:
-    def __init__(self, dic):
-        self.name = dic["name"]
-        self.steps = dic['steps']
-        self.working_directory = Path(dic['working_directory'])
-        self.datasets = [x for x in dic['datasets']]
-        self.exclude = [x for x in dic['exclude']]
-        self.train = ConfigTrain(dic['train'])
-        self.test = ConfigTest(dic['test'])
-        self.custom_annotations = dic['custom_annotations']
-        self.cpus = dic['cpus']
+    name: str
+    steps: list[str]
+    working_directory: Path
+    datasets: list[str]
+    exclude: list[str]
+    train: ConfigTrain
+    test: ConfigTest
+    custom_annotations: Path
+    cpus: int
+
+    # def __init__(self, dic):
+    #     self.name = dic["name"]
+    #     self.steps = dic['steps']
+    #     self.working_directory = Path(dic['working_directory'])
+    #     self.datasets = [x for x in dic['datasets']]
+    #     self.exclude = [x for x in dic['exclude']]
+    #     self.train = ConfigTrain(dic['train'])
+    #     self.test = ConfigTest(dic['test'])
+    #     self.custom_annotations = dic['custom_annotations']
+    #     self.cpus = dic['cpus']
 
 
 def _get_custom_annotations(path):
@@ -68,9 +81,18 @@ def _get_custom_annotations(path):
 def __main__(config_yaml="config.yaml"):
     # Initialize the config
     with open(config_yaml, "r") as f:
+        dic = yaml.safe_load(f)
+
         config = Config(
-            # **yaml.safe_load(f)
-            yaml.safe_load(f)
+            name=dic["name"],
+            steps=dic['steps'],
+            working_directory=Path(dic['working_directory']),
+            datasets=[x for x in dic['datasets']],
+            exclude=[x for x in dic['exclude']],
+            train=ConfigTrain(dic['train']),
+            test=ConfigTest(dic['test']),
+            custom_annotations=dic['custom_annotations'],
+            cpus=dic['cpus']
         )
         print(config)
 
@@ -84,7 +106,6 @@ def __main__(config_yaml="config.yaml"):
             custom_annotations: dict[tuple[str, str, int], bool] = _get_custom_annotations(config.custom_annotations)
             with open(custom_annotations_path, "wb") as f:
                 pickle.dump(custom_annotations, f)
-
 
     # Construct the dataset
     if "Collate" in config.steps:
