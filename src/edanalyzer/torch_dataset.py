987ac4076e2c408def7a0c33b6e6deccec2d2382
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-11-29 15:14:21 +0000

    Ligand scoring training

--- src/edanalyzer/torch_dataset.py
+++ src/edanalyzer/torch_dataset.py
@@ -1576,7 +1576,6 @@ def _get_transformed_ligand(event, sample_specification):  # Updates ligand_res
             rmsd = min([x for x in new_rmsds if x is not None])
             j+= 1
             if j > 200:
-                print()
                 print(f"Failed to sample a low RMSD pose! {new_rmsds}")
                 posed_ligand_res = None
                 sample_specification['annotation'] = False
