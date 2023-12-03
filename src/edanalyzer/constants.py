fa608d85f67ecf2915dffa91630aa233ccc8a778
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-12-03 17:19:13 +0000

    “

--- src/edanalyzer/constants.py
+++ src/edanalyzer/constants.py
@@ -56,6 +56,8 @@ PANDDA_EVENT_TABLE_PATH = "pandda_analyse_events.csv"
 PANDDA_SITE_TABLE_PATH = "pandda_analyse_sites.csv"
 PANDDA_ZMAP_TEMPLATE = "{dtag}-z_map.native.ccp4"
 PANDDA_GROUND_STATE_MAP_TEMPLATE = "{dtag}-ground-state-average-map.native.ccp4"
+PANDDA_LIGAND_FILES_DIR = "ligand_files"
+
 
 PANDDA_INSPECT_DTAG = "dtag"
 PANDDA_INSPECT_EVENT_IDX = "event_idx"
