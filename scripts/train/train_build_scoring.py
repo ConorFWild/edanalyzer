0ecb1cd8b15b827df977cfe3307a1e848ab81b48
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-02-09 10:05:46 +0000

    Major refactor

--- scripts/train/train_build_scoring.py
+++ scripts/train/train_build_scoring.py
@@ -1,15 +1,43 @@
+from pathlib import Path
+
 import fire
-
+import yaml
+from rich import print as rprint
 import lightning as lt
+import pony
 
-from edanalyzer.datasets.build_scoring import BuildScoringDataset
+from edanalyzer.datasets.build_scoring import BuildScoringDataset, BuildScoringDatasetItem
 from edanalyzer.models.build_scoring import LitBuildScoring
+from edanalyzer.data.database_schema import db, EventORM, AutobuildORM
+
+def main(config_path):
+    rprint(f'Running collate_database from config file: {config_path}')
+    # Load config
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
 
 
-def main():
+    # Get the Database
+    database_path = Path(config['working_directory']) / "database.db"
+    try:
+        db.bind(provider='sqlite', filename=f"{database_path}", create_db=True)
+        db.generate_mapping(create_tables=True)
+    except Exception as e:
+        print(f"Exception setting up database: {e}")
+
+    # Train
     trainer = lt.Trainer(accelerator='gpu')
-    dataset_train = BuildScoringDataset()
-    dataset_test = BuildScoringDataset()
+    with pony.orm.db_session:
+        query = [_x for _x in pony.orm.select(_y for _y in AutobuildORM)]
+        dataset_train = BuildScoringDataset(
+            [
+                BuildScoringDatasetItem(**_event.to_dict(exclude='id'))
+                for _event
+                in query
+                if _event['test_train'] == "Train"
+            ]
+        )
+        dataset_test = BuildScoringDataset()
     model = LitBuildScoring()
     trainer.fit(model, dataset_train, dataset_test)
 
