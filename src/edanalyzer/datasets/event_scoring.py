d101ad6dd667296c35bbc01007422b71c89484c1
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-02-12 16:13:22 +0000

    Major refactor

--- src/edanalyzer/datasets/event_scoring.py
+++ src/edanalyzer/datasets/event_scoring.py
@@ -60,7 +60,7 @@ class EventScoringDataset(Dataset):
 
         try:
             z_map = _load_xmap_from_path(sample.z_map)
-            ligand_array = _get_ligand_from_dir(Path(sample.event_map).parent.resolve()
+            ligand_array = _get_ligand_from_dir(Path(sample.event_map).parent.resolve())
             assert ligand_array.size > 0, f"Ligand array empty!"
         except Exception as e:
             print(e)
