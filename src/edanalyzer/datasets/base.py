68cc92658a79f26602509fe17e824ca1cd2f1739
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-02-28 10:24:34 +0000

    Major refactor

--- src/edanalyzer/datasets/base.py
+++ src/edanalyzer/datasets/base.py
@@ -320,7 +320,7 @@ def _load_xmap_from_mtz_path(path):
 
 def _load_xmap_from_path(path):
     ccp4 = gemmi.read_ccp4_map(str(path))
-    ccp4.setup(float('nan'))
+    ccp4.setup(0.0)
     m = ccp4.grid
 
     return m
