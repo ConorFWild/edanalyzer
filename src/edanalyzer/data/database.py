a1660c8d4ae778df7ac231091c3e51e85e85a92c
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-03-04 14:44:04 +0000

    Major refactor

--- src/edanalyzer/data/database.py
+++ src/edanalyzer/data/database.py
@@ -737,7 +737,7 @@ def _get_smiles(matched_cif):
 
 def _get_atom_ids(matched_cif):
     atom_ids = list(matched_cif[1].find_loop('_chem_comp_atom.atom_id'))
-    atom_types =  list(matched_cif[2].find_loop('_chem_comp_atom.type_symbol'))
+    atom_types =  list(matched_cif[1].find_loop('_chem_comp_atom.type_symbol'))
 
     return [_x for _x, _y in zip(atom_ids, atom_types) if _y != 'H']
 
