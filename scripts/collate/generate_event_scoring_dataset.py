90279190ac7525f8676e07b85b5bcbc2f4be0017
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-03-08 11:59:05 +0000

    “

--- scripts/collate/generate_event_scoring_dataset.py
+++ scripts/collate/generate_event_scoring_dataset.py
@@ -219,7 +219,7 @@ def main(config_path):
                     # )
                     new_grid = reference_frame.get_grid()
                     new_grid_array = np.array(new_grid, copy=False)
-                    new_grid_array[all_coords[:,0],all_coords[:,1],all_coords[:,2]] = resampling[:,:,:]
+                    new_grid_array[all_coords[:,0],all_coords[:,1],all_coords[:,2]] = resampling
                     rprint(resampling.shape)
                     rprint(reference_frame.spacing)
                     rprint(reference_frame.unit_cell)
