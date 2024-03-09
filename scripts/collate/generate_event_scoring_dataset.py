7aefae0549d11df1c38513814b3df803e73180a5
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-03-09 11:51:27 +0000

    “

--- scripts/collate/generate_event_scoring_dataset.py
+++ scripts/collate/generate_event_scoring_dataset.py
@@ -341,7 +341,7 @@ def main(config_path):
                     rprint(f'x,y,z reference shapes: {reference_frame.spacing}')
                     interp = RegularGridInterpolator((x, y, z), zmap_array)
 
-                    clipped_coords = np.clip(coordinate_array, np.array([0,0,0]).reshape(1,3), np.array(x[-1], y[-1], z[-1]).reshape(1,3))
+                    clipped_coords = np.clip(coordinate_array, np.array([0,0,0]).reshape(1,3), np.array([x[-1], y[-1], z[-1]]).reshape(1,3))
                     resampling = interp(clipped_coords)
                     rprint(resampling.shape)
 
