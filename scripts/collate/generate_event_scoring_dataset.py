06033e96faacb4c219aa94d0eeaf28d9d90daac9
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-03-09 11:37:05 +0000

    “

--- scripts/collate/generate_event_scoring_dataset.py
+++ scripts/collate/generate_event_scoring_dataset.py
@@ -55,6 +55,7 @@ def _get_close_events(
         delta=5.0
 ):
     distances = _get_close_distances(known_hit_centroid, experiment_hit_results)
+    rprint(distances)
 
     return [res for res, dis in zip(experiment_hit_results, distances) if dis < delta]
 
