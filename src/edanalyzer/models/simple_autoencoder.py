810c00e8b8b9a6667a07a9f6b66f16eea39210dc
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-03-20 15:27:13 +0000

    “

--- src/edanalyzer/models/simple_autoencoder.py
+++ src/edanalyzer/models/simple_autoencoder.py
@@ -14,7 +14,7 @@ class Block(nn.Module):
         self.conv = conv3x3(inplanes, outplanes, 2)
         self.bn = nn.BatchNorm3d(outplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.drop = nn.Dropout()
+        # self.drop = nn.Dropout()
         self.last = last
 
     def forward(self, x):
@@ -22,7 +22,7 @@ class Block(nn.Module):
         if not self.last:
             x = self.bn(x)
         x = self.relu(x)
-        x = self.drop(x)
+        # x = self.drop(x)
         return x
 
 
@@ -38,6 +38,7 @@ class SimpleConvolutionalEncoder(nn.Module):
         self.layer3 = Block(4, 8)
         self.layer4 = Block(8, 16)
         self.layer5 = Block(16, 32, last=True)
+        self.drop = nn.Dropout()
 
         # self.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
 
@@ -57,6 +58,7 @@ class SimpleConvolutionalEncoder(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.layer5(x)
+        x = self.drop(x)
 
         # x = self.avgpool(x)
         x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3] * x.shape[4])
@@ -76,14 +78,14 @@ class BlockTranspose(nn.Module):
         self.conv = convtranspose3x3(inplanes, outplanes, stride=2)
         self.bn = nn.BatchNorm3d(outplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.drop = nn.Dropout()
+        # self.drop = nn.Dropout()
 
     def forward(self, x):
         x = self.conv(x)
         # print(x.shape)
         x = self.bn(x)
         x = self.relu(x)
-        x = self.drop(x)
+        # x = self.drop(x)
         return x
 
 
@@ -94,6 +96,7 @@ class SimpleConvolutionalDecoder(nn.Module):
         self.input_layers = input_layers
 
         # Layers
+        self.drop = nn.Dropout()
         self.layer1 = BlockTranspose(input_layers, 16)
         self.layer2 = BlockTranspose(16, 8)
         self.layer3 = BlockTranspose(8, 4)
@@ -114,6 +117,7 @@ class SimpleConvolutionalDecoder(nn.Module):
         # for layer in self.layers:
         x = x.view(-1, self.input_layers, 1, 1, 1)
         x = self.avgpool(x)
+        x = self.drop(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
