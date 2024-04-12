1869bddd1be1f447bce7c0ded4ac1c640e85a0c4
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-04-12 15:39:14 +0100

    “

--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -226,7 +226,7 @@ class LitEventScoring(lt.LightningModule):
         return score
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
         return optimizer
 
     def training_step(self, train_batch, batch_idx):
