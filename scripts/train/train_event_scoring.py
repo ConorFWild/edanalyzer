d0d07d7387be3cfb09bca2b89a015a50cf2b4b68
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-04-23 18:22:46 +0100

    “

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -514,8 +514,8 @@ def main(config_path, batch_size=12, num_workers=None):
 
     # Train
     rprint('Constructing trainer...')
-    checkpoint_callback = ModelCheckpoint(dirpath='output/event_scoring_frag_no_neg_simple')
-    logger = CSVLogger("output/event_scoring_frag_no_neg_simple/logs")
+    checkpoint_callback = ModelCheckpoint(dirpath='output/event_scoring_frag_no_neg_fc')
+    logger = CSVLogger("output/event_scoring_frag_no_neg_fc/logs")
     trainer = lt.Trainer(accelerator='gpu', logger=logger,
                          callbacks=[
                              checkpoint_callback,
--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -216,7 +216,7 @@ class LitEventScoring(lt.LightningModule):
         )
         self.train_annotations = []
         self.test_annotations = []
-        self.output = Path('./output/event_scoring_frag_no_neg_simple')
+        self.output = Path('./output/event_scoring_frag_no_neg_fc')
 
     def forward(self, x, z, m, d):
         mol_encoding = self.mol_encoder(m)
