cb56d1ce822e9a289727444575f1c0d469f827f8
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-04-24 15:04:20 +0100

    “

--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -246,8 +246,8 @@ class LitEventScoring(lt.LightningModule):
         return score
 
     def configure_optimizers(self):
-        # optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
-        optimizer = torch.optim.SGD(self.parameters(), lr=1e-2)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        # optimizer = torch.optim.SGD(self.parameters(), lr=1e-2)
         # lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
         # return [optimizer], [lr_scheduler]
         # return {
