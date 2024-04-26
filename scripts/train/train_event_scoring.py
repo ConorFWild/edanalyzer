54e93fcd7f4b814ddc56923ab0680df35367ee06
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-04-26 13:33:54 +0100

    “

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -679,7 +679,7 @@ def main(config_path, batch_size=12, num_workers=None):
     # Train
     rprint('Constructing trainer...')
     checkpoint_callback = ModelCheckpoint(dirpath='output/event_scoring_frag_mask')
-    logger = CSVLogger("output/event_scoring_frag_sgd_fast_xl/logs")
+    logger = CSVLogger("output/event_scoring_frag_mask/logs")
     trainer = lt.Trainer(accelerator='gpu', logger=logger,
                          callbacks=[
                              checkpoint_callback,
