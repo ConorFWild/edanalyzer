bc42d327fe07b3749d956c322e61d276b49cb0f5
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-05 21:02:48 +0100

    Annotations as zarr and system, dtag and event_num included

--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -532,7 +532,7 @@ class LitEventScoring(lt.LightningModule):
 
         # If store: Load the store and group
         if store_file.exists():
-            root = zarr.open(store_file, mode='r')
+            root = zarr.open(store_file, mode='a')
             test_annotation_table = root['test_annotations']
 
         # Else: Create store and create groups
