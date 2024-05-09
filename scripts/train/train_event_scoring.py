262b67518fe91f597c92de0037c8cc752cf69909
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-09 10:02:36 +0100

    Annotations as zarr and system, dtag and event_num included

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -837,8 +837,8 @@ def main(config_path, batch_size=12, num_workers=None):
 
     # Train
     rprint('Constructing trainer...')
-    checkpoint_callback = ModelCheckpoint(dirpath='output/event_scoring_lig_annotations_fast')
-    logger = CSVLogger("output/event_scoring_lig_annotations_fast/logs")
+    checkpoint_callback = ModelCheckpoint(dirpath='output/event_scoring_lig_annotations_med')
+    logger = CSVLogger("output/event_scoring_lig_annotations_med/logs")
     trainer = lt.Trainer(accelerator='gpu', logger=logger,
                          callbacks=[
                              checkpoint_callback,
