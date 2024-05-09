601f4a1f2b0a0c69f47972b9c2cd36770acea72d
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-09 12:22:54 +0100

    Annotations as zarr and system, dtag and event_num included

--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -265,7 +265,7 @@ class LitEventScoring(lt.LightningModule):
 
     def configure_optimizers(self):
         # optimizer = torch.optim.Adam(self.parameters(), lr=1e-6)
-        optimizer = torch.optim.SGD(self.parameters(), lr=1e-1)
+        optimizer = torch.optim.SGD(self.parameters(), lr=1e-1, weight_decay=1e-2)
         # lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
         # return [optimizer], [lr_scheduler]
         # return {
