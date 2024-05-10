712abb0762d83af2e9d22110efa1c80f1381ced0
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-10 10:19:22 +0100

    Annotations as zarr and system, dtag and event_num included

--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -234,7 +234,7 @@ class LitEventScoring(lt.LightningModule):
         )
         self.train_annotations = []
         self.test_annotations = []
-        self.output = Path('./output/event_scoring_lig_annotations_adam_ls_slb')
+        self.output = Path('./output/event_scoring_lig_annotations_sgd_ls_slb')
 
     def forward(self, x, z, m, d):
         mol_encoding = self.mol_encoder(m)
