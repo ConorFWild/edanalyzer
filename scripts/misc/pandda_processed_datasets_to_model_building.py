7719eb69364331091f5a5167292fc0f6afbe82d6
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-23 15:53:37 +0100

    Annotations as zarr and system, dtag and event_num included

--- scripts/misc/pandda_processed_datasets_to_model_building.py
+++ scripts/misc/pandda_processed_datasets_to_model_building.py
@@ -7,7 +7,8 @@ import numpy as np
 import fire
 from rich import print as rprint
 
-OLD_SYSTEMS_PATH = Path('/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/analysis/pandda_2/pandda_autobuilding')
+OLD_SYSTEMS_PATH = Path(
+    '/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/analysis/pandda_2/pandda_autobuilding')
 
 DATA_DIRS = Path('/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/edanalyzer/data')
 
@@ -56,8 +57,61 @@ FORBIDDEN = [
     'KLHL7B'
 ]
 
+
+def process_dataset(system_name, experiment_dir):
+    # Make output data dir
+    data_dir = DATA_DIRS / system_name
+    if not data_dir.exists():
+        rprint(f'Making data dir: {data_dir}')
+        os.mkdir(data_dir)
+    else:
+        rprint(f'Already made: {data_dir}')
+
+    # Iterate pandda processed datasets
+    processed_datasets = experiment_dir / 'processed_datasets'
+    for dataset_dir in processed_datasets.glob('*'):
+        # Get dtag
+        dtag = dataset_dir.name
+
+        # Get the output data dataset dir
+        output_dataset_dir = data_dir / dtag
+        if not output_dataset_dir.exists():
+            rprint(f'Making data dataset dir: {output_dataset_dir}')
+            os.mkdir(output_dataset_dir)
+        else:
+            rprint(f'Already made dataset dir: {output_dataset_dir}')
+
+        # Find and copy pdb
+        pandda_pdb = dataset_dir / f'{dtag}-pandda-input.pdb'
+        data_pdb = output_dataset_dir / 'dimple.pdb'
+        if (not data_pdb.exists()) & (pandda_pdb.exists()):
+            rprint(f'Copying pandda pdb {pandda_pdb} -> {data_pdb}')
+            shutil.copy(pandda_pdb, data_pdb, follow_symlinks=True)
+        else:
+            rprint(f'Already copied {data_pdb}')
+
+        # Find and copy mtz
+        pandda_mtz = dataset_dir / f'{dtag}-pandda-input.mtz'
+        data_mtz = output_dataset_dir / 'dimple.mtz'
+        rprint(f'Copying pandda mtz {pandda_mtz} -> {data_mtz}')
+        if (not data_pdb.exists()) & (pandda_mtz.exists()):
+            shutil.copy(pandda_mtz, data_mtz, follow_symlinks=True)
+        else:
+            rprint(f'Already copied {data_mtz}')
+
+        # Find and copy ligand files
+        pandda_compound_dir = dataset_dir / f'ligand_files'
+        data_compound_dir = output_dataset_dir / 'compound'
+        if (not data_compound_dir.exists()) & (pandda_compound_dir.exists()):
+            rprint(f'Copying pandda compound dir {pandda_compound_dir} -> {data_compound_dir}')
+            shutil.copytree(pandda_compound_dir, data_compound_dir, symlinks=False)
+        else:
+            rprint(f'Already copied {data_mtz}')
+
+
 def main():
     new_systems = {}
+    datasets_to_process = []
     for experiment_dir in OLD_SYSTEMS_PATH.glob('*'):
         if not experiment_dir.is_dir():
             continue
@@ -77,57 +131,13 @@ def main():
         if system_name in FORBIDDEN:
             continue
 
-        # Make output data dir
-        data_dir = DATA_DIRS / system_name
-        if not data_dir.exists():
-            rprint(f'Making data dir: {data_dir}')
-            os.mkdir(data_dir)
-        else:
-            rprint(f'Already made: {data_dir}')
-
-        # Iterate pandda processed datasets
-        processed_datasets = experiment_dir / 'processed_datasets'
-        for dataset_dir in processed_datasets.glob('*'):
-            # Get dtag
-            dtag = dataset_dir.name
-
-            # Get the output data dataset dir
-            output_dataset_dir = data_dir / dtag
-            if not output_dataset_dir.exists():
-                rprint(f'Making data dataset dir: {output_dataset_dir}')
-                os.mkdir(output_dataset_dir)
-            else:
-                rprint(f'Already made dataset dir: {output_dataset_dir}')
-
-            # Find and copy pdb
-            pandda_pdb = dataset_dir / f'{dtag}-pandda-input.pdb'
-            data_pdb = output_dataset_dir / 'dimple.pdb'
-            if (not data_pdb.exists()) & (pandda_pdb.exists()):
-                rprint(f'Copying pandda pdb {pandda_pdb} -> {data_pdb}')
-                shutil.copy(pandda_pdb, data_pdb, follow_symlinks=True)
-            else:
-                rprint(f'Already copied {data_pdb}')
-
-            # Find and copy mtz
-            pandda_mtz = dataset_dir / f'{dtag}-pandda-input.mtz'
-            data_mtz = output_dataset_dir / 'dimple.mtz'
-            rprint(f'Copying pandda mtz {pandda_mtz} -> {data_mtz}')
-            if (not data_pdb.exists()) & (pandda_mtz.exists()):
-                shutil.copy(pandda_mtz, data_mtz, follow_symlinks=True)
-            else:
-                rprint(f'Already copied {data_mtz}')
-
-            # Find and copy ligand files
-            pandda_compound_dir = dataset_dir / f'ligand_files'
-            data_compound_dir = output_dataset_dir / 'compound'
-            if (not data_compound_dir.exists()) & (pandda_compound_dir.exists()):
-                rprint(f'Copying pandda compound dir {pandda_compound_dir} -> {data_compound_dir}')
-                shutil.copytree(pandda_compound_dir, data_compound_dir, symlinks=False)
-            else:
-                rprint(f'Already copied {data_mtz}')
+        datasets_to_process.append([system_name, experiment_dir])
 
         # new_systems.append(system_name)
 
+    from joblib import Parallel, delayed
+    Parallel(n_jobs=20, verbose=10)(
+        delayed(process_dataset)(system_name, experiment_dir) for system_name, experiment_dir in datasets_to_process)
 
     # unique_new_systems = [system for system in np.unique(new_systems) if system not in CURRENT_SYSTEMS]
     # print(unique_new_systems)
@@ -136,6 +146,7 @@ def main():
     #     print(system)
     # for project, system in new_systems.items():
     #     print(f'{project} : {system}')
+
 
 if __name__ == '__main__':
     fire.Fire(main)
