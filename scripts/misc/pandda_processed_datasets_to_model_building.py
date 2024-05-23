a061615cfb03082a84f48eed67317d81d3dd6b0d
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-23 13:31:40 +0100

    Annotations as zarr and system, dtag and event_num included

--- scripts/misc/pandda_processed_datasets_to_model_building.py
+++ scripts/misc/pandda_processed_datasets_to_model_building.py
@@ -3,8 +3,11 @@ from pathlib import Path
 
 import numpy as np
 import fire
+from rich import print as rprint
 
 OLD_SYSTEMS_PATH = Path('/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/analysis/pandda_2/pandda_autobuilding')
+
+DATA_DIRS = Path('/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/edanalyzer/data')
 
 CURRENT_SYSTEMS = [
     'A71EV2A',
@@ -60,14 +63,46 @@ def main():
         system_name = match.group(1)
         if system_name == 'refmac-from-coot-refmac-for':
             continue
-        new_systems.append(system_name)
+        if system_name in CURRENT_SYSTEMS:
+            continue
+
+        # Make output data dir
+        data_dir = DATA_DIRS / system_name
+        rprint(f'Would make data dir: {data_dir}')
+
+        # Iterate pandda processed datasets
+        processed_datasets = experiment_dir / 'processed_datasets'
+        for dataset_dir in processed_datasets.glob('*'):
+            # Get dtag
+            dtag = dataset_dir.name
+
+            # Get the output data dataset dir
+            output_dataset_dir = data_dir / dtag
+            rprint(f'Would make data dataset dir: {output_dataset_dir}')
+
+            # Find and copy pdb
+            pandda_pdb = dataset_dir / f'{dtag}-pandda-input.pdb'
+            data_pdb = output_dataset_dir / 'dimple.pdb'
+            rprint(f'Would copy pandda pdb {pandda_pdb} -> {data_pdb}')
+
+            # Find and copy mtz
+            pandda_mtz = dataset_dir / f'{dtag}-pandda-input.mtz'
+            data_mtz = output_dataset_dir / 'dimple.mtz'
+            rprint(f'Would copy pandda mtz {pandda_mtz} -> {data_mtz}')
+
+            # Find and copy ligand files
+            pandda_compound_dir = dataset_dir / f'ligand_files'
+            data_compound_dir =output_dataset_dir / 'compound'
+            rprint(f'Would copy pandda compound dir {pandda_compound_dir} -> {data_compound_dir}')
+
+        # new_systems.append(system_name)
 
 
-    unique_new_systems = [system for system in np.unique(new_systems) if system not in CURRENT_SYSTEMS]
-    print(unique_new_systems)
-    print(len(unique_new_systems))
-    for system in unique_new_systems:
-        print(system)
+    # unique_new_systems = [system for system in np.unique(new_systems) if system not in CURRENT_SYSTEMS]
+    # print(unique_new_systems)
+    # print(len(unique_new_systems))
+    # for system in unique_new_systems:
+    #     print(system)
 
 if __name__ == '__main__':
     fire.Fire(main)
