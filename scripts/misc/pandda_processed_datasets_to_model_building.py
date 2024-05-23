7d22b3ed531fae0a36cba75ce5bfa25841058712
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-23 11:59:22 +0100

    Annotations as zarr and system, dtag and event_num included

--- scripts/misc/pandda_processed_datasets_to_model_building.py
+++ scripts/misc/pandda_processed_datasets_to_model_building.py
@@ -1,6 +1,7 @@
 import re
 from pathlib import Path
 
+import numpy as np
 import fire
 
 OLD_SYSTEMS_PATH = Path('/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/analysis/pandda_2/pandda_autobuilding')
@@ -47,6 +48,7 @@ CURRENT_SYSTEMS = [
 ]
 
 def main():
+    new_systems = []
     for experiment_dir in OLD_SYSTEMS_PATH.glob('*'):
         if not experiment_dir.is_dir():
             continue
@@ -54,8 +56,12 @@ def main():
         match = re.match('system_(.*)_project', experiment_dir.name)
         if not match:
             continue
-        else:
-            print(match.group(1))
+
+        system_name = match.group(1)
+        new_systems.append(system_name)
+
+
+    print([system for system in np.unique(new_systems) if system not in CURRENT_SYSTEMS])
 
 if __name__ == '__main__':
     fire.Fire(main)
