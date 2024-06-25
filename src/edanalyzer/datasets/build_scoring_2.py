670d981a11274fc8de4c79e54f9e2c0f95a4ba19
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-06-25 11:37:07 +0100

    Annotations as zarr and system, dtag and event_num included

--- src/edanalyzer/datasets/build_scoring_2.py
+++ src/edanalyzer/datasets/build_scoring_2.py
@@ -261,14 +261,14 @@ class BuildScoringDataset(Dataset):
 
         rmsd = _decoy['rmsd']
         if _train:
-            if rmsd <= 1.5:
+            if rmsd < 1.5:
                 hit = [0.025, 0.975]
             elif rmsd >= 1.5:
                 hit = [0.975, 0.025]
             else:
                 raise Exception
         else:
-            if rmsd <= 1.5:
+            if rmsd < 1.5:
                 hit = [0.0, 1.0]
             elif rmsd >= 1.5:
                 hit = [1.0, 0.0]
