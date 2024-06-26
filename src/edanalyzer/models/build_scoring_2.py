c4523e3ce919fe9cfc207715e16efc15561d802f
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-06-26 13:17:33 +0100

    Annotations as zarr and system, dtag and event_num included

--- src/edanalyzer/models/build_scoring_2.py
+++ src/edanalyzer/models/build_scoring_2.py
@@ -69,7 +69,7 @@ class LitBuildScoring(lt.LightningModule):
         return score
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-3, weight_decay=1e-2)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-2, weight_decay=1e-2)
         # optimizer = torch.optim.SGD(self.parameters(), lr=1e-1, weight_decay=1e-2)
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, cooldown=10)
         # return [optimizer], [lr_scheduler]
