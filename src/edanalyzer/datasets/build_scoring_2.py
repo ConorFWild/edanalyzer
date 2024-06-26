35ab292dd98dbd5106cbe63878ca8be7dbe6a57d
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-06-26 16:17:07 +0100

    Annotations as zarr and system, dtag and event_num included

--- src/edanalyzer/datasets/build_scoring_2.py
+++ src/edanalyzer/datasets/build_scoring_2.py
@@ -97,7 +97,7 @@ def _get_overlap_volume(orientation, centroid, known_hit_pose_residue, decoy_res
     decoy_score_sample[decoy_score_sample >= 0.0] = 1.0
     decoy_score_sample[decoy_score_sample < 0.0] = 0.0
 
-    print({'known hit sample sum': np.sum(known_hit_score_sample})
+    print({'known hit sample sum': np.sum(known_hit_score_sample)})
 
 
     score = np.sum(known_hit_score_sample * decoy_score_sample) / np.sum(decoy_score_sample)
