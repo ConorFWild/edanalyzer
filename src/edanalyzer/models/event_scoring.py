99d41c0940c55d6d63c1f3c7faa3180c6292a0df
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-01-10 15:05:12 +0000

    Larger mask

--- src/edanalyzer/models/event_scoring.py
+++ src/edanalyzer/models/event_scoring.py
@@ -224,7 +224,7 @@ class LitEventScoring(lt.LightningModule):
             # nn.Linear(256,128),
             # nn.ReLU(inplace=True),
             # nn.Dropout(),
-            nn.Linear(512,2),
+            nn.Linear(512*2,2),
             # nn.BatchNorm1d(16),
             # nn.Linear(256,128),
             # nn.ReLU(inplace=True),
@@ -257,9 +257,9 @@ class LitEventScoring(lt.LightningModule):
         #     dim=1,
         # )
         # density_encoding = self.density_encoder(full_density)
-        # full_encoding = torch.cat([z_encoding, mol_encoding], dim=1)
+        full_encoding = torch.cat([z_encoding, mol_encoding], dim=1)
         # full_encoding = z_encoding * F.hardtanh(self.bn( self.mol_to_weight(mol_encoding)), min_val=-1.0, max_val=1.0)
-        full_encoding = z_encoding * F.hardtanh(mol_encoding, min_val=-1.0, max_val=1.0)
+        # full_encoding = z_encoding * F.hardtanh(mol_encoding, min_val=-1.0, max_val=1.0)
 
         score = F.softmax(self.fc(full_encoding))
 
@@ -311,9 +311,9 @@ class LitEventScoring(lt.LightningModule):
         #     dim=1,
         # )
         # density_encoding = self.density_encoder(full_density)
-        # full_encoding = torch.cat([z_encoding, mol_encoding], dim=1)
+        full_encoding = torch.cat([z_encoding, mol_encoding], dim=1)
         # full_encoding = z_encoding * F.hardtanh(self.bn( self.mol_to_weight(mol_encoding)), min_val=-1.0, max_val=1.0)
-        full_encoding = z_encoding * F.hardtanh(mol_encoding, min_val=-1.0, max_val=1.0)
+        # full_encoding = z_encoding * F.hardtanh(mol_encoding, min_val=-1.0, max_val=1.0)
 
 
         # score = F.sigmoid(self.fc(full_encoding))
@@ -375,9 +375,9 @@ class LitEventScoring(lt.LightningModule):
         # )
         # full_density = z
         # density_encoding = self.density_encoder(full_density)
-        # full_encoding = torch.cat([z_encoding, mol_encoding], dim=1)
+        full_encoding = torch.cat([z_encoding, mol_encoding], dim=1)
         # full_encoding =  z_encoding * F.hardtanh(self.bn( self.mol_to_weight(mol_encoding)), min_val=-1.0, max_val=1.0)
-        full_encoding = z_encoding * F.hardtanh(mol_encoding, min_val=-1.0, max_val=1.0)
+        # full_encoding = z_encoding * F.hardtanh(mol_encoding, min_val=-1.0, max_val=1.0)
 
         # print(f'Z Encoding: {z_encoding[0,:10]}')
         # print(f'Mol Encoding: {mol_encoding[0,:10]}')
