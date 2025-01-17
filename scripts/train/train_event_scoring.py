bc6cdb7100b2a3bfd515de8c47db8975849a9eae
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-01-17 13:34:12 +0000

    P

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -978,7 +978,7 @@ def main(config_path, batch_size=12, num_workers=None):
 
     # Get the model
     rprint('Constructing model...')
-    output = output_dir / 'event_scoring_prod_quantized_11'
+    output = output_dir / 'event_scoring_prod_quantized_12'
     model = LitEventScoring(output)
 
 
