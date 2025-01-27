5a6aed3314868426f18cc5ebbddaa4a711bff587
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-01-27 23:42:27 +0000

    P

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -1005,7 +1005,7 @@ def main(config_path, batch_size=12, num_workers=None):
 
     # Get the model
     rprint('Constructing model...')
-    study_name = 'event_scoring_prod_37'
+    study_name = 'event_scoring_prod_38'
     output = output_dir / study_name
 
     # Train
