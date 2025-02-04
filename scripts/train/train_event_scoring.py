1a12d129436b53508fc5b3674e924e611fe9eb53
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-02-04 12:04:10 +0000

    P

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -1017,7 +1017,7 @@ def main(config_path, batch_size=12, num_workers=None):
 
     # Get the model
     rprint('Constructing model...')
-    study_name = 'event_scoring_prod_46'
+    study_name = 'event_scoring_prod_47'
     output = output_dir / study_name
     if not output.exists():
         os.mkdir(output)
