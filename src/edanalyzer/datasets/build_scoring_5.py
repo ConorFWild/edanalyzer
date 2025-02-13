20f09e6f84f27548f7ff4bc4c924761d6ace568a
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-02-13 17:21:44 +0000

    Multi objective optimization

--- src/edanalyzer/datasets/build_scoring_5.py
+++ src/edanalyzer/datasets/build_scoring_5.py
@@ -195,7 +195,11 @@ class BuildScoringDataset(Dataset):
         # Get the metadata, decoy pose and embedding
         # _meta_idx, _decoy_idx, _embedding_idx, _train = sample_data['meta'], int(sample_data['decoy']), sample_data['embedding'], sample_data['train']
         _meta_idx = sample_data['meta']
-        _decoy_idx = int(sample_data['meta_to_decoy'].sample().iloc[0]['idx'])
+        try:
+            _decoy_idx = int(sample_data['meta_to_decoy'].sample().iloc[0]['idx'])
+        except:
+            print('meta to decoy')
+            print(sample_data['meta_to_decoy'])
         rprint(
             [
                 _meta_idx,
