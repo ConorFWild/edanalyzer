22c08b6045852c51c9a27ad3e879c536c8f385a0
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-02-17 14:31:45 +0000

    Multi objective optimization

--- scripts/collate/get_build_deltas_distribution.py
+++ scripts/collate/get_build_deltas_distribution.py
@@ -8,10 +8,14 @@ import pickle
 if __name__ == '__main__':
     zarr_path = '/dls/data2temp01/labxchem/data/2017/lb18145-17/processing/edanalyzer/output/build_data_augmented_2.zarr'
     root = zarr.open(zarr_path, mode='r')
+    rprint(f'Getting deltas')
     deltas = root['delta'].get_basic_selection(slice(None, None), fields='delta')
+    rprint('Got Deltas')
     bins = pd.cut(deltas[deltas>0.0], bins=np.linspace(0.0, 6.0, num=61))
     counts = bins.value_counts(dropna=False)
     with open('test.pickle', 'wb') as f:
         pickle.dump(bins, f)
     with open('test.pickle', 'rb') as f:
         df = pickle.load(f)
+
+    rprint('Saved and finished!')
