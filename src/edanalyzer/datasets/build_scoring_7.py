7e27a41658e903357c6820bcf34817943f777560
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-02-18 12:12:04 +0000

    Multi objective optimization

--- src/edanalyzer/datasets/build_scoring_7.py
+++ src/edanalyzer/datasets/build_scoring_7.py
@@ -249,13 +249,13 @@ class BuildScoringDataset(Dataset):
         valid_mask = _decoy['elements'] != 0
         valid_indicies = np.nonzero(valid_mask)
         random_drop_index = rng.integers(0, len(valid_indicies[0]))
-        print(f'Random drop index: {random_drop_index}')
+        # print(f'Random drop index: {random_drop_index}')
         drop_index = valid_indicies[0][random_drop_index]
         valid_poss = _decoy['positions'][(drop_index,),]
         valid_elements = _decoy['elements'][(drop_index,),]
 
         centroid = np.mean(valid_poss, axis=0)
-        print(f'Centroid: {centroid}')
+        # print(f'Centroid: {centroid}')
 
         sample_array = np.zeros(
             (32, 32, 32),
