ce561cb747ab65c79bad2274f951170fea1fc4f7
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-02-20 11:41:39 +0000

    Multi objective optimization

--- scripts/train/train_event_scoring.py
+++ scripts/train/train_event_scoring.py
@@ -1043,7 +1043,7 @@ def main(config_path, batch_size=12, num_workers=None):
 
     # Get the model
     rprint('Constructing model...')
-    study_name = 'event_scoring_prod_58'
+    study_name = 'event_scoring_prod_59'
     output = output_dir / study_name
     if not output.exists():
         os.mkdir(output)
