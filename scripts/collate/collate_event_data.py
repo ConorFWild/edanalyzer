72318655073aacf49aea578b76eed9eb7a3b54c3
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-08-14 11:29:42 +0100

    Event collation script

--- scripts/collate/collate_event_data.py
+++ scripts/collate/collate_event_data.py
@@ -336,6 +336,7 @@ def process_model_building_dir(model_building_dir, sqlite_path, pandda_dirs, tab
     print(sqlite_path)
     print(table)
     print(tables['CrystalName'])
+    print([x for x in table.iloc[0]])
     refinement_outcomes = table.loc[table.index, ['RefinementOutcome']]
     good_refinements = table[refinement_outcomes.isin(['4 - CompChem ready', '5 - Deposition ready', '6 - Deposited'])]
 
